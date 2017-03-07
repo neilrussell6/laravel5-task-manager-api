ed3c1dbd0f226fa01e120c919d38119eff94deb4
Neil Russell <neilrussell6@gmail.com>
2017-03-07 23:00:10 +0200

    Implement CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-
+        \Barryvdh\Cors\HandleCors::class
     ];
 
     /**
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'jsonapi' => \Neilrussell6\Laravel5JsonApi\Http\Middleware\JsonApi::class,
         'jsonapi.jwt' => \Neilrussell6\Laravel5JsonApi\Http\Middleware\TransformJWTResponse::class,
-//        'jsonapi.acl' => \Neilrussell6\Laravel5JsonApi\Http\Middleware\TransformACLResponse::class,
+        'jsonapi.acl' => \Neilrussell6\Laravel5JsonApi\Http\Middleware\TransformACLResponse::class,
         'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
         'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
         'role' => \Laratrust\Middleware\LaratrustRole::class,
--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
          */
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Laratrust\LaratrustServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Neilrussell6 Packages Service Providers...
